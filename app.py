8376fd657c36b4719294ae0c8afbbefab7be667c
TQ <35425439+TQgit@users.noreply.github.com>
2020-06-01 19:21:29 +0300

    updated endpoints and error handling

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask, request, abort, jsonify, render_template
 from flask_cors import CORS
-from models import setup_db
+from models import setup_db, Restaurant, Order
+from auth import requires_auth, AuthError
 
 
 def create_app(test_config=None):
@@ -22,9 +23,208 @@ def create_app(test_config=None):
         return render_template('index.html', login_url=os.environ['LOGIN_URL'],
                                github_url=os.environ['GITHUB_URL'])
 
+    @app.route('/restaurants')
+    @requires_auth('get:restaurants')
+    def get_restos(payload):
+        restaurants = [resto.format() for resto in Restaurant.query.all()]
+
+        return jsonify({'success': True,
+                        'restaurants': restaurants})
+
+    @app.route('/orders')
+    @requires_auth('get:orders')
+    def get_orders(payload):
+        orders = [order.format() for order in Order.query.all()]
+
+        return jsonify({'success': True,
+                        'orders': orders})
+
+    @app.route('/restaurants/<int:resto_id>/orders')
+    @requires_auth('get:orders')
+    def get_orders_of_resto(payload, resto_id):
+        orders = [order.format() for order in
+                  Order.query.filter_by(restaurant_id=resto_id).all()]
+        if not orders:
+            abort(404)
+
+        return jsonify({'success': True,
+                        'orders': orders})
+
+    @app.route('/restaurants', methods=['POST'])
+    @requires_auth('post:restaurants')
+    def post_restaurant(payload):
+        try:
+            data = request.get_json()
+
+            name = data['name']
+            cuisine = data['cuisine']
+
+            new_restaurant = Restaurant(name=name, cuisine=cuisine)
+            new_restaurant.insert()
+        except Exception:
+            abort(400)
+
+        return jsonify({'success': True,
+                        'restaurants': [new_restaurant.format()]})
+
+    @app.route('/orders', methods=['POST'])
+    @requires_auth('post:orders')
+    def post_order(payload):
+        try:
+            data = request.get_json()
+
+            resto_id = data['restaurant_id']
+            name = data['name']
+            calories = data['calories']
+            total_fat = data['total_fat']
+            cholesterol = data['cholesterol']
+            sodium = data['sodium']
+            total_carbs = data['total_carbs']
+            protein = data['protein']
+
+            new_order = Order(restaurant_id=resto_id, name=name,
+                              calories=calories, total_fat=total_fat,
+                              cholesterol=cholesterol, sodium=sodium,
+                              total_carbs=total_carbs, protein=protein)
+            new_order.insert()
+        except Exception:
+            abort(400)
+
+        return jsonify({'success': True,
+                        'orders': [new_order.format()]})
+
+    @app.route('/restaurants/<int:id>', methods=['PATCH'])
+    @requires_auth('patch:restaurants')
+    def patch_restaurant(payload, id):
+        resto = Restaurant.query.filter_by(id=id).one_or_none()
+        if resto is None:
+            abort(404)
+
+        try:
+            data = request.get_json()
+
+            if 'name' in data:
+                resto.name = data['name']
+
+            if 'cuisine' in data:
+                resto.cuisine = data['cuisine']
+
+            resto.update()
+        except Exception:
+            abort(400)
+
+        return jsonify({'success': True, 'restaurants': [resto.format()]})
+
+    @app.route('/orders/<int:id>', methods=['PATCH'])
+    @requires_auth('patch:orders')
+    def patch_order(payload, id):
+        order = Order.query.filter_by(id=id).one_or_none()
+        if order is None:
+            abort(404)
+
+        try:
+            data = request.get_json()
+            if 'name' in data:
+                order.name = data['name']
+            if 'calories' in data:
+                order.calories = data['calories']
+            if 'total_fat' in data:
+                order.total_fat = data['total_fat']
+            if 'cholesterol' in data:
+                order.cholesterol = data['cholesterol']
+            if 'sodium' in data:
+                order.sodium = data['sodium']
+            if 'total_carbs' in data:
+                order.total_carbs = data['total_carbs']
+            if 'protein' in data:
+                order.protein = data['protein']
+
+            order.update()
+        except Exception:
+            abort(400)
+
+        return jsonify({'success': True, 'orders': [order.format()]})
+
+    @app.route('/restaurants/<int:id>', methods=['DELETE'])
+    @requires_auth('delete:restaurants')
+    def delete_restaurant(payload, id):
+        resto = Restaurant.query.filter_by(id=id).one_or_none()
+        if resto is None:
+            abort(404)
+
+        try:
+            resto.delete()
+
+        except Exception:
+            abort(400)
+
+        return jsonify({'success': True, 'delete': id})
+
+    @app.route('/orders/<int:id>', methods=['DELETE'])
+    @requires_auth('delete:orders')
+    def delete_order(payload, id):
+        order = Order.query.filter_by(id=id).one_or_none()
+        if order is None:
+            abort(404)
+
+        try:
+            order.delete()
+
+        except Exception:
+            abort(400)
+
+        return jsonify({'success': True, 'delete': id})
+
     @app.route('/login-confirmation')
     def confirm_login():
         return render_template('login-confirm.html')
+
+    # Error Handling
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({'success': False,
+                        'error': 400,
+                        'message': 'bad request'
+                        }), 400
+
+    @app.errorhandler(405)
+    def not_allowed(error):
+        return jsonify({'success': False,
+                        'error': 405,
+                        'message': 'method not allowed'
+                        }), 405
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({'success': False,
+                        'error': 500,
+                        'message': 'Internal server error'
+                        }), 500
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+    @app.errorhandler(AuthError)
+    def authorization_error(AuthError):
+        return jsonify({
+            "success": False,
+            "error": AuthError.status_code,
+            "message": AuthError.error['description']
+        }), AuthError.status_code
 
     return app
 
